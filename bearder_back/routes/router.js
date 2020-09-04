7a6814aff39da00c8792f28bd7cac41738824061
p4p1 <p4p1@vivaldi.net>
2020-09-04 16:19:42 +0200

    plop

--- bearder_back/routes/router.js
+++ bearder_back/routes/router.js
@@ -10,7 +10,6 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '/.env') })
 
 const SECRETKEY = process.env.SECRETKEY; // md5 hash of Leo Smith
-const apikey = process.env.IBM_APIKEY; // IBM SECRET KEY FOR THE API
 
 router.post("/login", (req, res, next) => {
   // query to get username
--- bearder_front/src/App.js
+++ bearder_front/src/App.js
@@ -2,16 +2,73 @@ import React from 'react';
 import './style/App.css';
 import logo from './images/title_transparent.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Username: </p>
-        <p>Password: </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.getPassword = this.getPassword.bind(this);
+    this.getUsername = this.getUsername.bind(this);
+
+    global.api_link = "http://localhost:8080"
+  }
+
+  getUsername(input) {
+    this.setState({ username: input.target.value });
+    console.log(input.target.value);
+  }
+
+  getPassword(input) {
+    this.setState({ password: input.target.value });
+    console.log(input.target.value);
+  }
+
+  handleLogin() {
+    console.log('oui');
+    fetch(`${global.api_link}/login`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+    .then((response) => response.json())
+    .then((respJson) => {
+      console.log(respJson);
+      alert(respJson.msg);
+      global.api_token = respJson.token
+    }).catch((err) => console.error(err));
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <div>
+            <label>Username: </label>
+            <input type="text" onChange={this.getUsername} autoComplete="new-password" />
+          </div>
+          <div>
+            <label>Password: </label>
+            <input type="password" onChange={this.getPassword} autoComplete="new-password" />
+          </div>
+          <div class='Button-wrap'>
+            <input type="button" value='login' onClick={this.handleLogin} /><br />
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
